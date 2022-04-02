d5e187f47bc1c61c81fa0b94fefe7d3837800c4e
obulka <obulka@hotmail.com>
2022-04-01 23:29:41 -0700

    soft shadows

--- src/blink/sdTransforms.h
+++ src/blink/sdTransforms.h
@@ -7,34 +7,71 @@
 
 inline float union_(
         const float distance0,
-        const float distance1)
+        const float distance1,
+        const float4 &colour0,
+        float4 &colour1,
+        const float4 &surface0,
+        float4 &surface1)
 {
-    return min(distance0, distance1);
+    if (distance0 < distance1)
+    {
+        colour1 = colour0;
+        surface1 = surface0;
+        return distance0;
+    }
+    return distance1;
 }
 
 
 inline float subtraction(
         const float distance0,
-        const float distance1)
+        const float distance1,
+        const float4 &colour0,
+        float4 &colour1,
+        const float4 &surface0,
+        float4 &surface1)
 {
-    return max(-distance0, distance1);
+    if (-distance0 > distance1)
+    {
+        colour1 = colour0;
+        surface1 = surface0;
+        return -distance0;
+    }
+    return distance1;
 }
 
 
 inline float intersection(
         const float distance0,
-        const float distance1)
+        const float distance1,
+        const float4 &colour0,
+        float4 &colour1,
+        const float4 &surface0,
+        float4 &surface1)
 {
-    return max(distance0, distance1);
+    if (distance0 > distance1)
+    {
+        colour1 = colour0;
+        surface1 = surface0;
+        return distance0;
+    }
+    return distance1;
 }
 
 
 inline float smoothUnion(
         const float distance0,
         const float distance1,
+        const float4 &colour0,
+        float4 &colour1,
+        const float4 &surface0,
+        float4 &surface1,
         const float blendSize)
 {
     float amount = clamp(0.5f + 0.5f * (distance1 - distance0) / blendSize, 0.0f, 1.0f);
+
+    colour1 = blend(colour0, colour1, amount);
+    surface1 = blend(surface0, surface1, amount);
 
     return blend(distance0, distance1, amount) - blendSize * amount * (1.0f - amount);
 }
@@ -43,9 +80,16 @@ inline float smoothUnion(
 inline float smoothSubtraction(
         const float distance0,
         const float distance1,
+        const float4 &colour0,
+        float4 &colour1,
+        const float4 &surface0,
+        float4 &surface1,
         const float blendSize)
 {
     float amount = clamp(0.5f - 0.5f * (distance1 + distance0) / blendSize, 0.0f, 1.0f);
+
+    colour1 = blend(colour0, colour1, amount);
+    surface1 = blend(surface0, surface1, amount);
 
     return blend(-distance0, distance1, amount) + blendSize * amount * (1.0f - amount);
 }
@@ -54,9 +98,16 @@ inline float smoothSubtraction(
 inline float smoothIntersection(
         const float distance0,
         const float distance1,
+        const float4 &colour0,
+        float4 &colour1,
+        const float4 &surface0,
+        float4 &surface1,
         const float blendSize)
 {
     float amount = clamp(0.5f - 0.5f * (distance1 - distance0) / blendSize, 0.0f, 1.0f);
+
+    colour1 = blend(colour0, colour1, amount);
+    surface1 = blend(surface0, surface1, amount);
 
     return blend(distance0, distance1, amount) + blendSize * amount * (1.0f - amount);
 }
@@ -66,27 +117,76 @@ float performChildInteraction(
         const int modifications,
         const float distance0,
         const float distance1,
+        const float4 &colour0,
+        float4 &colour1,
+        const float4 &surface0,
+        float4 &surface1,
         const float blendSize)
 {
     if (modifications & 128)
     {
-        return subtraction(distance0, distance1);
+        return subtraction(
+            distance0,
+            distance1,
+            colour0,
+            colour1,
+            surface0,
+            surface1
+        );
     }
     if (modifications & 256)
     {
-        return intersection(distance0, distance1);
+        return intersection(
+            distance0,
+            distance1,
+            colour0,
+            colour1,
+            surface0,
+            surface1
+        );
     }
     if (modifications & 512)
     {
-        return smoothUnion(distance0, distance1, blendSize);
+        return smoothUnion(
+            distance0,
+            distance1,
+            colour0,
+            colour1,
+            surface0,
+            surface1,
+            blendSize
+        );
     }
     if (modifications & 1024)
     {
-        return smoothSubtraction(distance0, distance1, blendSize);
+        return smoothSubtraction(
+            distance0,
+            distance1,
+            colour0,
+            colour1,
+            surface0,
+            surface1,
+            blendSize
+        );
     }
     if (modifications & 2048)
     {
-        return smoothIntersection(distance0, distance1, blendSize);
+        return smoothIntersection(
+            distance0,
+            distance1,
+            colour0,
+            colour1,
+            surface0,
+            surface1,
+            blendSize
+        );
     }
-    return union_(distance0, distance1);
+    return union_(
+        distance0,
+        distance1,
+        colour0,
+        colour1,
+        surface0,
+        surface1
+    );
 }
